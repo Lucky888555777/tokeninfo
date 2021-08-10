9b651c3c2c6a88a982c77b3287b164d07092caa5
Lucky888555777 <79687844+Lucky888555777@users.noreply.github.com>
2021-08-10 16:08:37 +0800

    Update tokenInfo.js

--- tokenInfo.js
+++ tokenInfo.js
@@ -2,7 +2,7 @@
     "tokenHTIMPL":"0x7ADB8AF63b2d907aABFbE9FA8830E2c5Ec1eE0C1",
     "tokenAridrop":"0x803022965d927B0f81732C38869368235d5Ff40B",
     "tokenIMPL":"0xDcb3c91d6725D694a131727B2E3155A694111931",
-    "price":"Sale price:1KTO = 300000000000KTOMoon",
+    "price":"Sale price:1KTO = 3000000KTOMoon",
     "timeout":"2021-08-30",
     "MDEXSwap":"",
         
