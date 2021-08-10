cb8c71addbbf301249df6d287382504a9a2ebeeb
Lucky888555777 <79687844+Lucky888555777@users.noreply.github.com>
2021-08-10 13:47:20 +0800

    Update tokenInfo.js

--- tokenInfo.js
+++ tokenInfo.js
@@ -1,15 +1,15 @@
 {
-    "tokenHTIMPL":"0xC8ef6DeD1821FC6FD0cFB263C9514BF8a27D36C5",
-    "tokenAridrop":"0x578DE10Da4487378045b6bc45149E842bF189c42",
-    "tokenIMPL":"0x2e6FAB06aDa77D0559EfF704e503071C760B78B3",
-    "price":"Sale price:1HT = 300000000000HTIMPL",
-    "timeout":"2021-07-30",
-    "MDEXSwap":"Listing MDEXSwap 2021.09.15",
+    "tokenHTIMPL":"0xB6408e758f74Ff5E8683EEFcc289A06F2f7DfAB1",
+    "tokenAridrop":"0xDcb3c91d6725D694a131727B2E3155A694111931",
+    "tokenIMPL":"0xDcb3c91d6725D694a131727B2E3155A694111931",
+    "price":"Sale price:1KTO = 300000000000KTOMoon",
+    "timeout":"2021-08-30",
+    "MDEXSwap":"",
         
-    "IMPLSwap":"Listing IMPLSwap 2021.10.15",
-    "Twittter":"https://docs.impl.club",
-    "Telegram":"https://docs.impl.club",
-    "GitHub":"https://github.com/HTIMPL/Contract",
-    "GitBook":"https://docs.impl.club"
+    "IMPLSwap":"",
+    "Twittter":"",
+    "Telegram":"",
+    "GitHub":"",
+    "GitBook":""
 }
 
